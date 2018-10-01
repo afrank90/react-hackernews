3abc2b5136a5b17491638c35e8ecd72677e84b11
Arthur Frank <arthur.frank@boxuk.com>
2018-10-01 14:03:31 +0100

    [FEATURE] Adding PropTypes Adding properties type validation. That decrease chances of type mistake in future. Adjusted tests appropreately.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = ({ onClick, children, className = '' }) => (
+const Button = ({ onClick, children, className }) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
 );
+
+/**
+ * By using defaultProps we can define all default values for expected props above.
+ */
+Button.defaultProps = {
+  className: ''
+};
+
+/**
+ * Defining properties types.
+ * Some kind of equivalent to TypeScript for component properties.
+ */
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
 
 export default Button;
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../Button';
+import PropTypes from 'prop-types';
 
 /**
  * Table to render list of items.
@@ -29,5 +30,24 @@ const Table = ({ list, onDismiss }) => (
     ))}
   </div>
 );
+
+/**
+ * Defining properties types.
+ * Some kind of equivalent to TypeScript for component properties.
+ *
+ * In this example we can specify also type for expected array values.
+ */
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      author: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+      objectID: PropTypes.number.isRequired
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
 
 export default Table;
